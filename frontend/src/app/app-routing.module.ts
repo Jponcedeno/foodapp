ff9a6274e12df6280b30bdd4fab3afd3851192ea
Jeffrey Ponce <jponcedeno@gmail.com>
2022-10-25 16:09:58 -0400

    added search functionality using url params

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // empty string means home page
+  { path: '', component: HomeComponent },
+  //search is a literal text, anything after the colon is the route paramter
+  { path: 'search/:searchTerm', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
